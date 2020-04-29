08c6c71a87e8e4d5577025f04761ba2cec423f2e
lalit tanwar <lalit.tanwar258@gmail.com>
2020-04-29 17:15:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Sidebar from './Components/Sidebar';
+import Product from './Components/Product';
+import Topbar from './Components/Topbar';
+import Brand from './Components/Brand';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Topbar />
+        {/*  <Sidebar />
+      <Product /> */}
+        {/* <div className="container-fluid"> */}
+        <div className="row">
+          <div className="col-md-2" style={{paddingRight:0,paddingLeft:0}}>
+            <Sidebar />
+          </div>
+          <div className="col-md-10" style={{paddingRight:0,paddingLeft:0}}>
+
+            <div>
+              <Switch>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/product" exact component={Product} />
+                <Route path="/brand" exact component={Brand} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
