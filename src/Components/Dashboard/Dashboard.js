7e18c9e9dc5739b977ba4132d997414487f7ba58
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-01 19:08:05 +0530

    Project setup

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 export class Dashboard extends Component {
+
+    constructor(){
+        super();
+        this.setState({
+
+        });
+    }
+
+   componentDidMount(){
+       console.log(process.env.REACT_APP_API_BASE_URL);
+   }
+
     render() {
         return (
             <div style={{padding:10}}>
