46c7c5aae113814a16d660fb2666855d00b863f9
lalit tanwar <lalit.tanwar258@gmail.com>
2020-05-03 22:59:22 +0530

    Updates

---
+++ precache-manifest.8c19db3165ff9e339528cfcde500354c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba8ca281402b889a90d3b33015a00fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa1bddb35446148b6a9",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "686969cd83b511044adb",
+    "url": "/static/css/main.86f613eb.chunk.css"
+  },
+  {
+    "revision": "eaa1bddb35446148b6a9",
+    "url": "/static/js/2.a7847764.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a7847764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686969cd83b511044adb",
+    "url": "/static/js/main.97e7f8ea.chunk.js"
+  },
+  {
+    "revision": "2ed2c41d7e7afd316a28",
+    "url": "/static/js/runtime-main.daee509c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ims/precache-manifest.6950177f87532be3b7cea20e916e3257.js"
+  "/precache-manifest.8c19db3165ff9e339528cfcde500354c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ims/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
