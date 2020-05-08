d5c605200848c9f4b06cc66a83eb0c958a1f7b37
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-08 15:08:24 +0530

    token setup

--- src/Services/RequestService.js
+++ src/Services/RequestService.js
@@ -8,8 +8,8 @@ class RequestService extends React.Component {
     }
 
     getRequest = function (url) {
-        const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDb0l6N3IxckJvM0N3dTd5c3A2SnJRPT0iLCJjb250YWN0IjoiMkY0ZDJIanpzd0VoangrWmszSGlyQT09IiwibmFtZSI6Ik5hcnBhdCIsImlkIjoxLCJleHAiOjE1ODg4ODE4NzksImlhdCI6MTU4ODg2Mzg3OSwiZW1haWwiOiJDb0l6N3IxckJvM0N3dTd5c3A2SnJRPT0ifQ.6otY1ITtC6QWHENZ1RV8BRu1M3EeBiqeVkZNetpZyCQRO6CdPNOqbugfhpmuUik_2Mwp8rX95v6Ww1e5zIp97g'
-        const headers = { Authorization: `Bearer ${token}` };
+        const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDb0l6N3IxckJvM0N3dTd5c3A2SnJRPT0iLCJjb250YWN0IjoiMkY0ZDJIanpzd0VoangrWmszSGlyQT09IiwibmFtZSI6Ik5hcnBhdCIsImlkIjoxLCJleHAiOjE1ODg4ODE4NzksImlhdCI6MTU4ODg2Mzg3OSwiZW1haWwiOiJDb0l6N3IxckJvM0N3dTd5c3A2SnJRPT0ifQ.6otY1ITtC6QWHENZ1RV8BRu1M3EeBiqeVkZNetpZyCQRO6CdPNOqbugfhpmuUik_2Mwp8rX95v6Ww1e5zIp97g';
+        const headers = { 'token': `${token}` };
         console.log('headers',headers)
         return axios.get(url, {headers})
             .then(response => { return Promise.resolve(response.data) })
